cf344d907d4b2a9791fdba232930af84b828f302
RinKit6024 <butasan6024@gmail.com>
2022-07-30 20:32:01 +0900

    Strings added with some assertion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(non_snake_case)]
 //mod print;
 //mod vars;
-mod types;
+//mod types;
+mod strings;
 
 fn main() {
     //println!("Hello, world!");
     //print::run(); //running the fuction called "run()" from "print.rs"
     //vars::run();
-    types::run();
-    
+    //types::run();
+    strings::run();
+     
 }
---
+++ src/strings.rs
@@ -0,0 +1,77 @@
+// Primitive str = Immutable fixed-length string somewhere in memory
+// String = Growable, heap-allocated data structure 
+//- Use when you need to modify or own string data
+// Parenthesis are not required when creating.
+
+pub fn run() {
+    //Allocate to heap with string::from and make it mutable
+    // &str can't edit, essentially same as "&'static str"
+    let mut hello = String::from("hello "); //make it heap allocated. 
+
+    //get length
+    //Allocate to 
+    println!("length: {}", hello.len());
+    println!("{}", hello);
+
+    //Push char
+    hello.push('W');
+
+    println!("{}", hello);
+
+    //Push String
+    hello.push_str("orld!");
+
+    println!("{}", hello);
+
+    //ex
+    //let mut hello2: &str = ("hello2");
+    //hello2.push('W'); 
+    //This will give you a error as &str can't be editted as it is only pointing the reference of its where string is stored.
+
+    //Capacity in bytes
+    println!("Capacity: {}", hello.capacity());
+
+    //Check if empty: Produce bool value "true" if it's empty
+    println!("Is Empty: {}", hello.is_empty());
+
+    //Contains: Case sensitive
+    println!("Contains World: {}", hello.contains("World"));
+
+    //Replace
+    println!("Replace: {}", hello.replace("World", "There"));
+
+    //Loop through string by whitespace
+    //split at where space is located
+    for word in hello.split_whitespace(){
+        println!("{}", word);
+    }
+
+    //Create string with capacity
+    let mut s = String::with_capacity(10);
+    s.push('a');
+    s.push('b');
+    //nth iterates through the string and return the Option<T> 
+    //Option<T> is a value that 
+    //As iterator may or may not have next value, .nth will return the Option<T>
+    //Option<T> consists either Some() (there is a value) or none (nothing in the T)
+    //To print the Option<T> value, you must use Debug traits !dbg() or {:?}
+    println!("{:?}", s.chars().nth(1));
+    dbg!(s.chars().nth(1));
+
+    //If you would like to use Display traits and show actual UTF-8 value,
+    //you must use .unwrap()
+
+    println!("{}", s.chars().next().unwrap());
+
+    //Assertion testing
+    let mut b: bool = true;
+    assert!(b); //It doesn't do anything which means no error ;)
+    b = false;
+    //assert!(b); If false, this assertion will panic.
+
+    //If both variables are equal then it's fine otherwise it gives error.
+    assert_eq!(2, s.len());
+    assert_eq!(10, s.capacity());
+
+
+}
